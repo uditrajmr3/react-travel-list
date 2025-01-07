cbe66424b0ff9ba7564fe5b2df6a50de55227374
uditrajmr3 <csense2653@gmail.com>
2025-01-07 11:17:01 +0530

    --updated: round off bug

--- src/Components/Stats.js
+++ src/Components/Stats.js
@@ -3,7 +3,7 @@ export default function Stats({ itemCount, packedCount }) {
     <footer className="stats">
       <em>
         🧳 You have {itemCount} items on your list, and you have already packed{" "}
-        {packedCount} ({(packedCount / itemCount) * 100}%)
+        {packedCount} ({Math.round((packedCount / itemCount) * 100)}%)
       </em>
     </footer>
   );
